c7700b2a6f17b6e15d37eb35997f5074bab4efdc
JustBeingAbdi <73824306+JustAbdi@users.noreply.github.com>
2020-12-30 22:09:52 +0100

    Pushed a new Update
    Added channels object and Mmembers object more updates is coming
    
    -Merge Closed-

--- DataTypes/Guild.js
+++ DataTypes/Guild.js
@@ -1,9 +1,11 @@
 module.exports = class Guild {
-    constructor(id, name, roles)
+    constructor(id, name, roles, channels, members)
     {
         this.id = id;
         this.name = name;
         this.roles = roles;
+        this.channels = channels;
+        this.members = members;
         Object.freeze(this);
     }
 }
